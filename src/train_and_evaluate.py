10aba63dc97bf3cbe261296c06c7821fd259ab1b
Prash M <prash16@gmail.com>
2021-03-18 17:14:13 -0700

    changes

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -78,10 +78,10 @@ def train_and_evaluate(config_path):
 #####################################################
 
 
-    # os.makedirs(model_dir, exist_ok=True)
-    # model_path = os.path.join(model_dir, "model.joblib")
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
 
-    # joblib.dump(lr, model_path)
+    joblib.dump(lr, model_path)
 
 
 
