b10405f8f08b9dfd18b1e1af297f77640fe09dce
Prash M <prash16@gmail.com>
2021-03-21 10:01:23 -0700

    update horkulu

--- src/get_data.py
+++ src/get_data.py
@@ -19,7 +19,7 @@ def get_data(config_path):
     #print(df.head())
     return df
 
-### experimenting 
+### experimenting .
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
